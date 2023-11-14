fea61159fb289a08db969252e60866088d3ae5d4
JSebastianBe <sebastianbm97@gmail.com>
2023-11-14 13:30:54 -0500

    Ajuste en nav-bar y login

--- Views/Template/header_Home.php
+++ Views/Template/header_Home.php
@@ -1,3 +1,6 @@
+<?php
+$infoPagePreguntas = !empty(GetPageRout('preguntas-frecuentes')) ? GetPageRout('preguntas-frecuentes') : "";
+?>
 
 <!DOCTYPE html>
 <html lang="en">
@@ -27,6 +30,31 @@
 	<link rel="stylesheet" type="text/css" href="<?= media(); ?>/matchpet/css/style.css">
 </head>
 <body class="animsition">
+
+
+	<!-- Modal -->
+	<div class="modal fade" id="modalAyuda" tabindex="-1" aria-hidden="true">
+	<div class="modal-dialog modal-lg">
+		<div class="modal-content">
+		<div class="modal-header">
+			<h5 class="modal-title"><?= $infoPagePreguntas['titulo']  ?></h5>
+			<button type="button" class="close" data-dismiss="modal" aria-label="Close">
+			<span aria-hidden="true">&times;</span>
+			</button>
+		</div>
+		<div class="modal-body">
+			<div class="page-content">
+				
+				<?= $infoPagePreguntas['contenido']  ?>
+			</div>
+		</div>
+		<div class="modal-footer">
+			<button type="button" class="btn btn-secondary" data-dismiss="modal">Cerrar</button>
+		</div>
+		</div>
+	</div>
+	</div>
+
 	<div id="divLoading">
       <div>
         <img src="<?=media();?>/images/loading.svg" alt="Loading">
@@ -37,41 +65,54 @@
 	<!-- Header desktop -->
 		<div class="container-menu-desktop">
 		
-		<?php
-		if(isset($_SESSION['login']))
-		{
-		?>	
 			<!-- Topbar -->
 			<div class="top-bar">
 				<div class="content-topbar flex-sb-m h-full container">
 					<div class="left-top-bar">
-						Bienvenido usuario: <?=$_SESSION['userData']['nombres']?> <?=$_SESSION['userData']['apellidos']?>
+						<?php
+							if(isset($_SESSION['login']))
+							{
+							?>	
+								Bienvenido usuario: <?=$_SESSION['userData']['nombres']?> <?=$_SESSION['userData']['apellidos']?>
+							<?php
+							}
+							?>
 					</div>
 
 					<div class="right-top-bar flex-w h-full">
-						<a href="#" class="flex-c-m trans-04 p-lr-25">
-							Help & FAQs
+						<a href="#" class="flex-c-m trans-04 p-lr-25" data-target="#modalAyuda" data-toggle="modal">
+							Ayuda
 						</a>
-
-						<a href="<?=BaseUrl();?>/Usuarios/perfil" class="flex-c-m trans-04 p-lr-25">
+						<?php
+							if(isset($_SESSION['login']))
+							{
+						?>	
+						<a href="<?=BaseUrl();?>/dashboard" class="flex-c-m trans-04 p-lr-25">
 							Mi cuenta
 						</a>
-
+						<?php
+							}
+						?>
+						<?php
+							if(isset($_SESSION['login']))
+							{
+						?>	
 						<a href="<?=BaseUrl();?>/Logout" class="flex-c-m trans-04 p-lr-25">
 							Salir
 						</a>
+						<?php
+							}else{
+						?>
+						<a href="<?=BaseUrl();?>/login" class="flex-c-m trans-04 p-lr-25">
+							Iniciar Sesión
+						</a>
+						<?php
+							}
+						?>
 					</div>
 				</div>
 			</div>
 			<div class="wrap-menu-desktop">
-		<?php
-		}else
-		{
-		?>
-			<div class="wrap-menu-desktop" style="top: 0px;">		
-		<?php
-		}
-		?>
 				<nav class="limiter-menu-desktop container">
 					
 					<!-- Logo desktop -->		
@@ -136,8 +177,8 @@
 
 				<li>
 					<div class="right-top-bar flex-w h-full">
-						<a href="#" class="flex-c-m p-lr-10 trans-04">
-							Help & FAQs
+						<a href="#" data-toggle="modal" data-target="#modalAyuda" class="flex-c-m trans-04 p-lr-25">
+							Ayuda
 						</a>
 
 						<a href="<?=BaseUrl();?>/Usuarios/perfil" class="flex-c-m p-lr-10 trans-04">
